46798e5aa6ac9e88b97e022493d19e7a2a3e51e3
AnushkaM02 <anushka.mulik@gmail.com>
2023-04-07 16:21:51 +0530

    mongoose connection and model creation

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,12 @@
 const asyncHandler = require("express-async-handler");
 
+const Goal = require("../models/goalModel");
+
 // Get goals, GET, /api/goals, Private
 
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get goals" });
+  const goals = await Goal.find();
+  res.status(200).json(goals);
 });
 
 // Set goal, POST /api/goals, Private
